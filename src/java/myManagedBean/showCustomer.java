5a1b1e8d49946ec13e754214259e1a9e51caef75
JoshuaHeth <13061283@studentmail.ul.ie>
2016-04-23 19:26:55 +0100

    Josh and Ankit work

---
+++ src/java/myManagedBean/showCustomer.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myManagedBean;
+
+import EntitiesPackage.Customer;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Vish
+ */
+@Named(value = "showCustomer")
+@RequestScoped
+public class showCustomer {
+
+    @PersistenceContext(unitName = "Group13ProjectPU")
+    private EntityManager em;
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+
+    
+    
+    private String custName;
+    private int custID;
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public int getCustID() {
+        return custID;
+    }
+
+    public void setCustID(int custID) {
+        this.custID = custID;
+    }
+    
+    public List<Customer> getCustomerByName() {
+
+        // create named query and set parameter
+        Query query = em.createNamedQuery("Customer.findByName")
+                .setParameter("name", custName);
+        // return query result
+        return query.getResultList();
+    }
+    
+     public List<Customer> getCustomerByID() {
+
+        // create named query and set parameter
+        Query query = em.createNamedQuery("Customer.findByCustomerId")
+                .setParameter("customerId", custID);
+        // return query result
+        return query.getResultList();
+    }
+     
+     public List<Customer> getAllCustomers() {
+
+        // create named query and set parameter
+        Query query = em.createNamedQuery("Customer.findAll");
+        // return query result
+        return query.getResultList();
+    }
+    
+}
